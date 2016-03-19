9c12b896dadfb03c0f4e6ed5c1a919f143af9c4a
Josh Gillespie <josh@bethel.io>
2016-03-19 13:56:02 -0500

    Updates links, fixes page height

--- _site/resume.php
+++ _site/resume.php
@@ -1,5 +1,5 @@
 <?php
 	header("Content-disposition: attachment; filename=resume.pdf");
 	header("Content-type: application/pdf");
-	readfile("static/files/resume.pdf");
+	readfile("assets/files/resume.pdf");
 ?>
